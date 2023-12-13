64503dc908ea955ae200dc03252a2e1bbbf2b036
charlinleo <ccharlin@student.ciputra.ac.id>
2023-12-13 16:16:06 +0700

    1

--- app/src/main/java/com/example/alp_vp_dev1/view/LoginView.kt
+++ app/src/main/java/com/example/alp_vp_dev1/view/LoginView.kt
@@ -1,13 +1,17 @@
 package com.example.alp_vp_dev1.view
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -31,31 +35,44 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.sp
 import com.example.alp_vp_dev1.ui.theme.DarkGrey
 import com.example.alp_vp_dev1.ui.theme.IjoButton
+import kotlinx.coroutines.launch
+import java.util.regex.Pattern
 import kotlin.math.round
 
-
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginView() {
 
+    var name by rememberSaveable { mutableStateOf("") }
+    var phone by rememberSaveable { mutableStateOf("") }
     var email by rememberSaveable { mutableStateOf("") }
     var password by rememberSaveable { mutableStateOf("") }
+
+    var isEmailValid by rememberSaveable { mutableStateOf(true) }
     var isPasswordValid by rememberSaveable { mutableStateOf(true) }
 
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
 
     Scaffold(
         content = {
             Column(
                 modifier = Modifier
-                    .padding(it)
                     .fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
@@ -63,9 +80,50 @@ fun LoginView() {
                 Text(
                     text = "Login",
                     fontSize = 32.sp,
-                    fontWeight = FontWeight.Bold
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
                 )
                 Spacer(modifier = Modifier.height(32.dp))
+                OutlinedTextField(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 8.dp),
+                    value = name,
+                    onValueChange = { name = it },
+                    label = {
+                        Text(text = "Full Name")
+                    },
+                    placeholder = {
+                        Text(text = "Full Name")
+                    },
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        unfocusedBorderColor = Color(0xFF333138),
+                        focusedBorderColor = Color(0xFFD0FF00)
+                    ),
+                    shape = RoundedCornerShape(12.dp)
+                )
+                OutlinedTextField(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 8.dp),
+                    value = phone,
+                    onValueChange = { phone = it },
+                    label = {
+                        Text(text = "Phone")
+                    },
+                    placeholder = {
+                        Text(text = "Phone")
+                    },
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Phone,
+                        imeAction = ImeAction.Next
+                    ),
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        unfocusedBorderColor = Color(0xFF333138),
+                        focusedBorderColor = Color(0xFFD0FF00)
+                    ),
+                    shape = RoundedCornerShape(12.dp)
+                )
                 OutlinedTextField(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -97,10 +155,64 @@ fun LoginView() {
                         .padding(horizontal = 16.dp, vertical = 8.dp),
                     isPasswordValid = isPasswordValid
                 )
+                Button(
+                    onClick = {
+                        isEmailValid = isValidEmail(email)
+                        isPasswordValid = isValidPassword(password)
 
+                        if (isEmailValid && isPasswordValid) {
+                            scope.launch {
+                                snackbarHostState.showSnackbar("Data $name saved")
+                            }
+                        }
+                    },
+                    colors = ButtonDefaults.buttonColors(Color(0xFFD0FF00)),
+                    shape = RoundedCornerShape(15.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 16.dp)
+                        .shadow(18.dp, shape = CircleShape),
+//                    enabled = name.isNotBlank() && phone.isNotBlank() && email.isNotBlank() && password.isNotBlank(),
+
+                ) {
+                    Text(
+                        text = "Login",
+                        color = Color.Black
+                    )
+                }
+                Spacer(modifier = Modifier.height(20.dp))
+                Row {
+                    Divider(
+                        color = Color.LightGray,
+                        modifier = Modifier
+                            .padding(horizontal = 20.dp)
+                            .width(40.dp)
+                    )
+                    Text(
+                        text = "Atau login dengan",
+                        color = Color.LightGray
+                    )
+                }
             }
         }
     )
+}
+
+// Function to validate email using regex
+fun isValidEmail(email: String): Boolean {
+    val emailPattern = Pattern.compile(
+        "^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$",
+        Pattern.CASE_INSENSITIVE
+    )
+    return emailPattern.matcher(email).matches()
+}
+
+// Function to validate password
+fun isValidPassword(password: String): Boolean {
+    val passwordPattern = Pattern.compile(
+        "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+\$).{8,}\$"
+    )
+    return passwordPattern.matcher(password).matches()
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
