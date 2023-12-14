c96db42be085a491d4c844008430f76b5dedeaac
charlinleo <ccharlin@student.ciputra.ac.id>
2023-12-14 14:33:04 +0700

    2

--- app/src/main/java/com/example/alp_vp_dev1/view/LoginView.kt
+++ app/src/main/java/com/example/alp_vp_dev1/view/LoginView.kt
@@ -1,11 +1,14 @@
 package com.example.alp_vp_dev1.view
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -15,43 +18,44 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.alp_vp_dev1.ui.theme.DarkGrey
-import com.example.alp_vp_dev1.ui.theme.IjoButton
+import com.example.alp_vp_dev1.R
 import kotlinx.coroutines.launch
 import java.util.regex.Pattern
-import kotlin.math.round
+
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
@@ -155,6 +159,19 @@ fun LoginView() {
                         .padding(horizontal = 16.dp, vertical = 8.dp),
                     isPasswordValid = isPasswordValid
                 )
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 8.dp)
+                        .clickable {  },
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    Text(
+                        text = "Lupa Password?",
+                    )
+                }
+
                 Button(
                     onClick = {
                         isEmailValid = isValidEmail(email)
@@ -167,32 +184,99 @@ fun LoginView() {
                         }
                     },
                     colors = ButtonDefaults.buttonColors(Color(0xFFD0FF00)),
-                    shape = RoundedCornerShape(15.dp),
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(horizontal = 16.dp, vertical = 16.dp)
-                        .shadow(18.dp, shape = CircleShape),
-//                    enabled = name.isNotBlank() && phone.isNotBlank() && email.isNotBlank() && password.isNotBlank(),
-
+                        .height(50.dp)
+                        .shadow(18.dp, shape = RoundedCornerShape(5.dp)),
+                    shape = RoundedCornerShape(4.dp),
+                    enabled = name.isNotBlank() && phone.isNotBlank() && email.isNotBlank() && password.isNotBlank(),
                 ) {
                     Text(
                         text = "Login",
                         color = Color.Black
                     )
                 }
-                Spacer(modifier = Modifier.height(20.dp))
-                Row {
+                Spacer(modifier = Modifier.height(10.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
                     Divider(
                         color = Color.LightGray,
                         modifier = Modifier
-                            .padding(horizontal = 20.dp)
-                            .width(40.dp)
+                            .absolutePadding(left = 20.dp)
+                            .width(100.dp)
                     )
                     Text(
                         text = "Atau login dengan",
                         color = Color.LightGray
                     )
+                    Divider(
+                            color = Color.LightGray,
+                    modifier = Modifier
+                        .absolutePadding(right = 20.dp)
+                        .width(100.dp)
+                    )
                 }
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                ) {
+                    Button(
+                        onClick = {
+
+                        },
+                        colors = ButtonDefaults.buttonColors(Color.White),
+                        modifier = Modifier
+                            .width(200.dp)
+                            .padding(horizontal = 16.dp, vertical = 16.dp)
+                            .height(50.dp)
+                            .border(1.dp, Color.LightGray, shape = RoundedCornerShape(5.dp)),
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.google_icon),
+                            contentDescription = "Google"
+                        )
+                        Text(
+                            text = "Google",
+                            color = Color.Black
+                        )
+                    }
+                    Button(
+                        onClick = {
+
+                        },
+                        colors = ButtonDefaults.buttonColors(Color.White),
+                        modifier = Modifier
+                            .width(200.dp)
+                            .padding(horizontal = 16.dp, vertical = 16.dp)
+                            .height(50.dp)
+                            .border(1.dp, Color.LightGray, shape = RoundedCornerShape(5.dp)),
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.facebook_icon),
+                            contentDescription = "Facebook",
+                            modifier = Modifier
+                                .clip(shape = CircleShape)
+                        )
+                        Text(
+                            text = "Facebook",
+                            color = Color.Black
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.height(5.dp))
+                Row {
+                    Text(text = "belum memliki akun?")
+                    Text(
+                        text = " Register",
+                        color = Color(0xFFA4C904),
+                        modifier = Modifier
+                            .clickable {  }
+                    )
+                }
+
             }
         }
     )
